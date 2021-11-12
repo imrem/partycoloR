ebc0c54650c4c62ba6daee6fce2a23d2ffb49011
Michael Imre <michael.imre@uni-mannheim.de>
2021-11-12 15:42:48 +0100

    Update partycolor.R

--- R/partycolor.R
+++ R/partycolor.R
@@ -6,8 +6,7 @@
 #' Default is hex.
 #' @param websafe logical: should web safe colors be returned instead? Old browsers were not able to display
 #' other colors than these, with current browsers this option should not be necessary, default is FALSE.
-#' @param include_ids logical: should party facts ids be included in the output? If the colors get added to an existing data
-#' frame that already has the ids this is usually not helpful, default is FALSE.
+#' @param include_ids logical: should party facts ids be included in the output? Default is TRUE
 #' @param include_description logical: should the name of the color (e.g. "Dark blue") be included in the output? 
 #' Default is FALSE.
 #' @param include_source logical: should the data source of the color be included in the output? Can be wikidata or
@@ -30,7 +29,7 @@
 partycolor <- function(ids, 
                     type = c("hex","rgb","cmyk","all"),
                     websafe=FALSE,
-                    include_ids=FALSE,
+                    include_ids=TRUE,
                     include_description=FALSE,
                     include_source=FALSE,
                     include_multiple=FALSE) {
